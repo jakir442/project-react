d40c7b8aa73992e9ea6132829855e43ffd659244
jakir442 <jakigeming2021@gmail.com>
2023-11-16 18:47:07 +0700

    complete update user functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,11 +8,12 @@ import { set } from 'mongoose';
 
 export default function Profile() {
   const fileRef = useRef(null);
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePerc, setFilePerc] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
   const dispatch = useDispatch();
 
   //firebase storage
@@ -97,12 +98,16 @@ export default function Profile() {
           <input type="text" placeholder="username" defaultValue={currentUser.username} id="username" className="border p-3 rounded-lg" onChange={handleChange} />
           <input type="email" placeholder="email" defaultValue={currentUser.email} id="email" className="border p-3 rounded-lg" onChange={handleChange} />
           <input type="password" placeholder="password" id="password" className="border p-3 rounded-lg" onChange={handleChange} />
-          <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">update</button>
+          <button disabled={loading} className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+            {loading ? 'Loading...' : 'Update'}
+          </button>
         </form>
         <div className="flex justify-between mt-5">
           <span className="text-red-700 cursor-pointer">Delete account</span>
           <span className="text-red-700 cursor-pointer">Sign out</span>
         </div>
+        <p className="text-red-700 mt-5">{error ? error : ''}</p>
+        <p className="text-green-700 mt-5">{updateSuccess ? 'User is updated successfully!' : ''}</p>
       </div>
     </div>
   );
