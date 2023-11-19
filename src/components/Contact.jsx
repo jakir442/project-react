4652958cfa928928153094062c3755119aa6900e
jakir442 <jakigeming2021@gmail.com>
2023-11-19 12:35:02 +0700

    Add contact landlord functionality to the listing page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,28 +23,18 @@ export default function Contact({ listing }) {
   return (
     <>
       {landlord && (
-        <div className='flex flex-col gap-2'>
+        <div className="flex flex-col gap-2">
           <p>
-            Contact <span className='font-semibold'>{landlord.username}</span>{' '}
-            for{' '}
-            <span className='font-semibold'>{listing.name.toLowerCase()}</span>
+            Contact <span className="font-semibold">{landlord.username}</span> for <span className="font-semibold">{listing.name.toLowerCase()}</span>
           </p>
-          <textarea
-            name='message'
-            id='message'
-            rows='2'
-            value={message}
-            onChange={onChange}
-            placeholder='Enter your message here...'
-            className='w-full border p-3 rounded-lg'
-          ></textarea>
+          <textarea name="message" id="message" rows="2" value={message} onChange={onChange} placeholder="Enter your message here..." className="w-full border p-3 rounded-lg"></textarea>
 
-          <Link
-          to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-          className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
-          >
-            Send Message          
+          <Link to={`mailto:jakigeming2021@gmail.com?subject=Regarding ${listing.name}&body=${message}`} className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95">
+            Send Message
           </Link>
+          {/* <Link to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`} className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95">
+            Send Message
+          </Link> */}
         </div>
       )}
     </>
