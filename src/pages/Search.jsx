b963541313cf2331afb13679ff54a5939cce4eed
jakir442 <jakigeming2021@gmail.com>
2023-11-19 19:04:28 +0700

    add show more listings functionality

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ListingItem from '../components/ListingItem';
-// import ListingItem from '../components/ListingItem';
 
 export default function Search() {
   const navigate = useNavigate();
@@ -98,6 +97,20 @@ export default function Search() {
     navigate(`/search?${searchQuery}`);
   };
 
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await res.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
+  };
+
   return (
     <div className="flex flex-col md:flex-row">
       <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
@@ -154,6 +167,11 @@ export default function Search() {
           {!loading && listings.length === 0 && <p className="text-xl text-slate-700">No listing found!</p>}
           {loading && <p className="text-xl text-slate-700 text-center w-full">Loading...</p>}
           {!loading && listings && listings.map((listing) => <ListingItem key={listing._id} listing={listing} />)}
+          {showMore && (
+            <button type="button" onClick={onShowMoreClick} className="text-green-700 hover:underline p-7 text-center w-full">
+              Show more
+            </button>
+          )}
         </div>
       </div>
     </div>
